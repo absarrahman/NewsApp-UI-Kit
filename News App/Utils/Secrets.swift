f88890c1fbeeb11b142b36af4726734db68d4a87
absarrahman <raman040635@gmail.com>
2023-01-12 00:50:49 +0600

    utilis: Add class for secret keys
    Signed-off-by: absarrahman <raman040635@gmail.com>

---
+++ News App/Utils/Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  Secrets.swift
+//  News App
+//
+//  Created by Moh. Absar Rahman on 12/1/23.
+//
+
+import Foundation
+
+// CLASS FOR SECRET KEYS
+class Secret {
+    static let apiKey = ""
+}
